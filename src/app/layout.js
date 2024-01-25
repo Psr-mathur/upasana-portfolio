2b859e357b25c3b1b71083cfe282496f9c826030
Prakash Mathur <Prakashkumarggh@gmail.com>
2024-01-25 17:00:06 +0530

    Add OpenGraph metadata to layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,21 @@
 import './globals.css';
 import { Inconsolata } from 'next/font/google';
 
+/**
+ * @type {import('next').Metadata}
+ */
 export const metadata = {
 	title: 'Upasana Rawat',
 	description: 'Portfolio of Upasana Rawat.',
+	openGraph: {
+		type: 'website',
+		locale: 'en_US',
+		url: 'https://upasana.vercel.app/',
+		images: ['https://upasana.vercel.app/opengraph-image.png'],
+		site_name: 'Upasana Rawat',
+		title: 'Upasana Rawat',
+		description: 'Portfolio of Upasana Rawat.',
+	},
 };
 
 export const inconsolata = Inconsolata({
