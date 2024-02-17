856b6ccdcb9305330366794cf0ee54382f22e98e
Prakash Mathur <Prakashkumarggh@gmail.com>
2024-02-17 11:53:17 +0530

    Update animation properties in word-in-char.jsx

--- src/framerAnimation/word-in-char.jsx
+++ src/framerAnimation/word-in-char.jsx
@@ -5,17 +5,21 @@ export function WordInChar({ word = 'DESIGNER' }) {
 	const charAni = {
 		hidden: (i) => ({
 			opacity: 0,
-			y: 25,
+			y: 50,
+			rotateX: 90,
 		}),
 		visible: (i) => ({
 			opacity: 1,
 			y: 0,
+			rotateX: 0,
 			transition: {
 				delay: i * 0.2,
 				duration: 0.5,
 				ease: 'easeInOut',
 				type: 'spring',
 				stiffness: 100,
+				repeat: Infinity,
+				repeatDelay: 5,
 			},
 		}),
 	};
